91637b9cbacc59062458f1dc46dfea5df74c5160
bbljo22 <167366077+bbljo22@users.noreply.github.com>
2024-07-08 13:14:37 +0900

    second commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: 'https://github.com/aaljo222/portfolio2', // replace 'your-repo-name' with your actual repository name
+    base: 'https://bbljo22.github.io/portfolio3/', // replace 'your-repo-name' with your actual repository name
   plugins: [react()],
   server: {
     port: 3000, // you can change the port if needed
